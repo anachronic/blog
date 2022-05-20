9ba98768e654c33154d50bf4de656c7e679ef1fe
Nicolás Salas <nsalas@fastmail.com>
2022-05-20 19:25:12 -0400

    feat: update SEO for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import * as matter from 'gray-matter'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkHtml from 'remark-html'
+import { Helmet } from 'react-helmet-async'
 
 interface Props {
   title: string
@@ -28,11 +29,17 @@ export async function getStaticProps() {
 
 const AboutPage: React.FC<Props> = ({ title, htmlContent }) => {
   return (
-    <Layout>
-      <h1>{title}</h1>
+    <React.Fragment>
+      <Helmet>
+        <title>About | Nicolás Salas V.</title>
+      </Helmet>
 
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
-    </Layout>
+      <Layout>
+        <h1>{title}</h1>
+
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
+      </Layout>
+    </React.Fragment>
   )
 }
 
