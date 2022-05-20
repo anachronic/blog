25ce24a497c4006939b4012fc6541a619465ed39
Nicolás Salas <nsalas@fastmail.com>
2022-05-20 17:57:03 -0400

    chore: move index to next.js
    This being a chore commit, aims to mostly set up next.js without keeping
    the annoying boilerplate. Some stuff was moved, but will need more work
    to move everything

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+      © {new Date().getFullYear()},
+      Nicolás Salas V.
+      <a href="https://github.com/anachronic">@anachronic</a>
     </footer>
   )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,23 @@
-import { Link } from 'gatsby'
 import React from 'react'
 import { IoLogoGithub } from 'react-icons/io'
+import Link from 'next/link'
 
 export const Header: React.FC = () => {
   return (
     <header className="navbar-top">
       <div className="lhs">
-        <Link className="item" to="/">
+        <Link className="item" href="/">
           Home
         </Link>
       </div>
       <div className="middle">
-        <Link className="item" to="/articles">
+        <Link className="item" href="/articles">
           Articles
         </Link>
-        <Link className="item" to="/about">
+        <Link className="item" href="/about">
           About
         </Link>
-        <Link className="item" to="/wiki">
+        <Link className="item" href="/wiki">
           Wiki
         </Link>
       </div>
