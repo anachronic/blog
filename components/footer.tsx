7b6359dfbc533f475cba7c18230f137ec9ff1878
Nicolás Salas <nsalas@fastmail.com>
2022-05-20 18:11:37 -0400

    style: reformat everything

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()},
-      Nicolás Salas V.
+      © {new Date().getFullYear()}, Nicolás Salas V.
       <a href="https://github.com/anachronic">@anachronic</a>
     </footer>
   )
