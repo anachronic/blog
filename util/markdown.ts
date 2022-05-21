893db627dc5431e99a1e72ae1aec90ff6cb76a7c
Nicolás Salas <nsalas@fastmail.com>
2022-05-20 23:59:07 -0400

    feat: sort articles by date desc

--- util/markdown.ts
+++ util/markdown.ts
@@ -5,6 +5,11 @@ import { join, sep } from 'path'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkHtml from 'remark-html'
+import { format } from 'date-fns'
+
+function articleSorter(article1: Article, article2: Article) {
+  return new Date(article2.date).getTime() - new Date(article1.date).getTime()
+}
 
 export async function readArticles(basePath: string) {
   const dir = await opendir(basePath)
@@ -15,14 +20,22 @@ export async function readArticles(basePath: string) {
     const filePath = join(basePath, markdownFile.name)
     const fileMatter = matter.read(filePath)
 
+    let date = fileMatter.data.date
+
+    if (date) {
+      date = format(date, 'yyyy/MM/dd')
+    } else {
+      date = 'No date found for article 😞'
+    }
+
     articles.push({
-      date: fileMatter.data.date.toString(),
+      date,
       slug: markdownFile.name.replaceAll(/\.md$/g, ''),
       title: fileMatter.data.title,
     })
   }
 
-  return articles
+  return articles.sort(articleSorter)
 }
 
 export async function readArticle(path: string) {
@@ -36,10 +49,18 @@ export async function readArticle(path: string) {
   const pathSegments = path.split(sep)
   const slug = pathSegments[pathSegments.length - 1].replaceAll(/\.md$/g, '')
 
+  let date = file.data.date
+
+  if (date) {
+    date = format(date, 'yyyy/dd/MM')
+  } else {
+    date = 'No date found for article 😞'
+  }
+
   return {
     slug,
+    date,
     title: file.data.title.toString(),
-    date: file.data.date?.toString() || 'No date found for article 😞',
     htmlContent: parsedContent.value.toString(),
   }
 }
