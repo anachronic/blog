94bd98fb7f9117a080bfa41ba9f8a86664af9b3e
Nicolás Salas <nsalas@fastmail.com>
2022-08-29 00:33:53 -0400

    refactor: move body to styled component

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import { Body } from '../lib/body'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+
+      <Body>
+        <Main />
+        <NextScript />
+      </Body>
+    </Html>
+  )
+}
