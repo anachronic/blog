f27922bd6a4d49643173687d6dcacf054aa8bf4e
Nicolás Salas <nsalas@fastmail.com>
2022-08-28 23:55:04 -0400

    refactor: move footer to emotion

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Footer as LibFooter } from '../lib/footer'
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      © {new Date().getFullYear()}, Nicolás Salas V.
+    <LibFooter>
+      © 2020 - {new Date().getFullYear()}, Nicolás Salas V.
       <a href="https://github.com/anachronic">@anachronic</a>
-    </footer>
+    </LibFooter>
   )
 }
---
+++ lib/footer.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+
+export const Footer = styled.footer`
+  text-align: center;
+  margin-top: 2em;
+  margin-bottom: 1em;
+`
