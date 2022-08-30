73230bf0958e265d2035e2897e7dd08370b27fca
Nicolás Salas <nsalas@fastmail.com>
2022-08-29 22:40:49 -0400

    refactor: move global styles to emotion Global

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { Body } from '../lib/body'
+import { Global, css } from '@emotion/react'
+
+const globalStyles = css`
+  a {
+    text-decoration: none;
+    color: #3273dc;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`
 
 export default function Document() {
   return (
     <Html>
       <Head />
+
+      <Global styles={globalStyles} />
 
       <Body>
         <Main />
