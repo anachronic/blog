d066af5e65eef99b6e8c685b80617037a2c28b91
Nicolás Salas <nsalas@fastmail.com>
2022-08-29 22:45:35 -0400

    refactor: move container code to emotion

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,23 @@
 import React, { PropsWithChildren } from 'react'
 import { Header } from './header'
 import { Footer } from './footer'
+import styled from '@emotion/styled'
+import { Container } from '../lib/container'
+
+
+const MainContainer = styled(Container)`
+  margin-top: 1rem;
+  font-size: 1.5em;
+`
 
 export const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <>
       <Header />
 
-      <div className="container main">
+      <MainContainer>
         <main>{children}</main>
-      </div>
+      </MainContainer>
 
       <Footer />
     </>
