e3613a0768d05bfd1e376f14dfbf2248f54a62b7
Nicolás Salas <nsalas@fastmail.com>
2023-05-30 00:34:49 -0400

    next: remove everything

--- lib/body.tsx
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Body = styled.body`
-  margin: 0;
-  font-family: Avenir, Arial, Helvetica, sans-serif;
-  line-height: 1.5;
-`
--- lib/container.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Container = styled.div`
-  margin: 0 auto;
-  max-width: 960px;
-`
